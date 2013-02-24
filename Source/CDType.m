nygard/class-dump

Fix parsing of the type-encodings for blocks and function pointers.
The type-encoding "@?" means "block", and "^?" means "function pointer".
Unfortunately there is no way to determine the original parameter types
or return type of the function or block, so I arbitrarily pick
(void(*)()) and (void(^)()) as the type displayed by class-dump.

Source/CDType.m
@@ -151,6 +151,24 @@ static BOOL debugMerge = NO;
     return self;
 }
 
+- (id)initFunctionPointerType
+{
+    if ((self = [self init])) {
+        type = T_FUNCTION_POINTER_TYPE;
+    }
+
+    return self;
+}
+
+- (id)initBlockType
+{
+    if ((self = [self init])) {
+        type = T_BLOCK_TYPE;
+    }
+
+    return self;
+}
+
 - (id)initModifier:(int)aModifier type:(CDType *)aType;
 {
     if ((self = [self init])) {
@@ -395,6 +413,20 @@ static BOOL debugMerge = NO;
                 result = [NSString stringWithFormat:@"(%@)", result];
             
             result = [subtype formattedString:result formatter:typeFormatter level:level];
+            break;
+            
+        case T_FUNCTION_POINTER_TYPE:
+            if (currentName == nil)
+                result = @"void(*)()";
+            else
+                result = [NSString stringWithFormat:@"void(*%@)()", currentName];
+            break;
+            
+        case T_BLOCK_TYPE:
+            if (currentName == nil)
+                result = @"void(^)()";
+            else
+                result = [NSString stringWithFormat:@"void(^%@)()", currentName];
             break;
             
         case 'j':

wenbobao/PPiOS-Rename

Source/CDType.m
@@ -184,6 +184,24 @@ static BOOL debugMerge = NO;
     return self;
 }
 
+- (id)initFunctionPointerType
+{
+    if ((self = [self init])) {
+        _primitiveType = T_FUNCTION_POINTER_TYPE;
+    }
+
+    return self;
+}
+
+- (id)initBlockType
+{
+    if ((self = [self init])) {
+        _primitiveType = T_BLOCK_TYPE;
+    }
+
+    return self;
+}
+
 - (id)initModifier:(int)modifier type:(CDType *)type;
 {
     if ((self = [self init])) {
@@ -411,6 +429,20 @@ static BOOL debugMerge = NO;
                 result = [NSString stringWithFormat:@"(%@)", result];
             
             result = [self.subtype formattedString:result formatter:typeFormatter level:level];
+            break;
+            
+        case T_FUNCTION_POINTER_TYPE:
+            if (currentName == nil)
+                result = @"void(*)()";
+            else
+                result = [NSString stringWithFormat:@"void(*%@)()", currentName];
+            break;
+            
+        case T_BLOCK_TYPE:
+            if (currentName == nil)
+                result = @"void(^)()";
+            else
+                result = [NSString stringWithFormat:@"void(^%@)()", currentName];
             break;
             
         case 'j':