e004cc9bc1f9e6a953abfaa029f7bca2fcbbb03e
Mike Richter <mrichter@preemptive.com>
2016-04-20 09:55:26 -0400

    Add crash method to test app #14682

---
+++ test/apps/BoxSim/BoxSim/TextDisplay.h
@@ -0,0 +1,10 @@
+// Copyright 2016 PreEmptive Solutions LLC. All rights reserved.
+
+#ifndef TextDisplay_h
+#define TextDisplay_h
+
+@interface TextDisplay : NSObject
+- (void)write:(NSString *)text;
+@end
+
+#endif // TextDisplay_h
