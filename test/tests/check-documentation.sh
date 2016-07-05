c7bc677b5d57eebb10cf7f3d5556246051926508
Mike Richter <mrichter@preemptive.com>
2016-07-05 16:02:10 -0400

    Check varied lenght version numbers #15071

--- test/tests/check-documentation.sh
+++ test/tests/check-documentation.sh
@@ -31,9 +31,9 @@ verifyFails test -z "${NUMERIC_VERSION}"
 # Find all of the version numbers "x.y.z" in the document, and put them on separate lines.
 # The two lines that follow that have trailing backslashes are newlines embedded in the sed
 # replacement text, do not indent or otherwise alter.
-versionNumbers=$(cat "${README}" | sed -n 's,\([1-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*\),\
+versionNumbers=$(cat "${README}" | sed -n 's,\([1-9][0-9]*[.][0-9][0-9]*\([.][0-9][0-9]*\)*\),\
 \1\
-,pg' | grep '[1-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*')
+,pg' | grep '[1-9][0-9]*[.][0-9][0-9]*\([.][0-9][0-9]*\)*')
 
 # Remove all of the instances of the expected version number
 badVersionNumbers="$(echo "${versionNumbers}" | grep -v "$(echo "^${NUMERIC_VERSION}\$" | sed 's,[.],[.],g')")"
